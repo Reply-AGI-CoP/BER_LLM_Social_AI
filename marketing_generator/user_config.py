a221371e152b23d09eb2eb77851a7148f69adba7
dmytromeshcheriakov <dmmesher@gmail.com>
2023-12-05 11:38:16 +0100

    Update user_config.py

--- marketing_generator/user_config.py
+++ marketing_generator/user_config.py
@@ -1,5 +1,6 @@
 import os
 import openai
+import streamlit as st
 from dotenv import load_dotenv
 
 
@@ -14,12 +15,15 @@ def setup_api_keys():
     load_dotenv()
 
     # Retrieve and set the OpenAI API key from the environment variable
-    os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
-    openai.api_key = os.getenv('OPENAI_API_KEY')
+    # os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
+    # os.environ['OPENAI_API_KEY'] = st.secrets('OPENAI_API_KEY')
+    # openai.api_key = os.getenv('OPENAI_API_KEY')
+    openai.api_key = st.secrets('OPENAI_API_KEY')
 
     # Retrieve Stable Diffusion API key, Host and Engine
     engine_id = "stable-diffusion-xl-1024-v1-0" # The older Model is: "stable-diffusion-v1-5" -> Cheaper for Testing purposes
     api_host = os.getenv('API_HOST', 'https://api.stability.ai')
-    sd_api_key = os.getenv("STABILITY_API_KEY")
+    # sd_api_key = os.getenv("STABILITY_API_KEY")
+    sd_api_key = st.secrets("STABILITY_API_KEY")
 
     return engine_id, api_host, sd_api_key
